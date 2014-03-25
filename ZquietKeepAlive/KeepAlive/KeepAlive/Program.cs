7b5e59a5d7ae7f4f4d67f9a0c3af02d76ae7702c
CONVERSION\mhaeri <CONVERSION\mhaeri>
2014-03-25 01:40:11 +0000

    Zquiet.com ping to keep app pool alive.

---
+++ ZquietKeepAlive/KeepAlive/KeepAlive/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Net;
+
+namespace KeepAlive
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+            PingURLs();
+        }
+
+
+        private static void PingURLs()
+        {
+            try
+            {
+                Console.WriteLine("Starting to ping urls:");
+                string[] urls = KeepAlive.Properties.Settings.Default.RequestURL.Split(new char[] { '|' });
+
+                foreach (string url in urls)
+                {
+                    Console.WriteLine(string.Format("Requesting: {0}",url));
+                    WebRequest request = HttpWebRequest.Create(url);
+                    WebResponse response = request.GetResponse();
+                    Console.WriteLine(string.Format("Request was successful, conect length: {0}", response.ContentLength.ToString()));
+                }
+                Console.WriteLine("Exiting application.");
+                Application.Exit();
+            }
+            catch (Exception)
+            {
+            }
+
+        }
+    }
+}
